bf0b2b961bf13f63424bef3bc17c7e936a215bf9
EAGLESWIN-LP-01\Senthil <senthil@embrava.com>
2018-09-26 17:27:33 +0530

    11

--- genesys/embrava_premium_client_app/wizard/scripts/config.js
+++ genesys/embrava_premium_client_app/wizard/scripts/config.js
@@ -1,6 +1,6 @@
 export default {
     clientIDs: {
-        'mypurecloud.com': 'ea4ad669-c893-4a52-93e6-d23f28487506'
+        'mypurecloud.com': 'e93d91c6-481d-4238-835d-566c425aab46'
         //'mypurecloud.ie': '8233c135-c488-427c-b301-158b164f2e4d',
         //'mypurecloud.com.au': '173ed139-b09c-4189-b206-97ef3dce4d16'
     },
