fe531e788ba3320c885186baea4decacb7a4835c
EAGLESWIN-LP-01\Senthil <senthil@embrava.com>
2018-09-29 02:00:29 +0530

    2

--- genesys/embrava_premium_client_app/wizard/scripts/wizard-app.js
+++ genesys/embrava_premium_client_app/wizard/scripts/wizard-app.js
@@ -61,7 +61,7 @@ class WizardApp {
             "appInstances": [
                 {
                     "name": "Application",
-                    "url": "https://mypurecloud.github.io/purecloud-premium-app/index.html?lang={{pcLangTag}}&environment={{pcEnvironment}}",
+                    "url": "https://embrava.github.io/genesys/embrava_premium_client_app/index.html?lang={{pcLangTag}}&environment={{pcEnvironment}}",
                     "type": "standalone",
                     "groups": ["Agents"]
                 }
