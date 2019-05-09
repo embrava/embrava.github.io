ab77972efcc188cac68e3eb79a8b264440cc2f66
EAGLESWIN-LP-01\Senthil <senthil@embrava.com>
2019-05-09 09:06:07 +0530

    Testing

--- genesys/embrava_premium_client_app2/wizard/scripts/config.js
+++ genesys/embrava_premium_client_app2/wizard/scripts/config.js
@@ -4,7 +4,7 @@ export default {
         'mypurecloud.ie': '41c93e94-ba9e-4c5e-bf9a-f9ee0b043420',
         'mypurecloud.com.au': 'd08762cb-d5fd-4447-88ae-c138a374ce31'
     },
-    "redirectUri": "https://embrava.github.io/genesys/embrava_premium_client_app/wizard/index.html",
+    "redirectUri": "https://embrava.github.io/genesys/embrava_premium_client_app2/wizard/index.html",
     
     //Permissions required for running the Wizard App
     "setupPermissionsRequired": ['admin'],
