edcfa907936eaa67c77aa28b02e2e340f7c1fc2a
embrava <43605449+embrava@users.noreply.github.com>
2019-08-19 09:20:45 +0530

    removed copy

--- genesys/embrava_premium_client_app - Copy/clientIDs.js
+++
@@ -1,6 +0,0 @@
-export default {
-    'mypurecloud.com': 'ea4ad669-c893-4a52-93e6-d23f28487506',
-    'mypurecloud.ie': '96e1c1b3-bbd5-43c2-bb88-11552884001b',
-    'mypurecloud.com.au': 'adddd327-0027-4714-b807-d61d8cad7a1b',
-	'mypurecloud.de': '41be7c60-d8f0-4f13-be10-b5e55d4d9b4f'
-}
