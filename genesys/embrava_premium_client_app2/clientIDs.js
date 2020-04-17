66ff80940bdd0e54283af72b477709f5b17bfb69
embrava <43605449+embrava@users.noreply.github.com>
2020-04-17 21:12:23 +0530

    Update Wizard

--- genesys/embrava_premium_client_app2/clientIDs.js
+++ genesys/embrava_premium_client_app2/clientIDs.js
@@ -1,5 +1,6 @@
 export default {
     'mypurecloud.com': 'bab69ff9-7e90-4765-9237-daeb4956134c',
     'mypurecloud.ie': '77b877d9-31cc-45b3-aec0-4cbf9a10f201',
-    'mypurecloud.com.au': '5783fd83-07c9-4f91-83b0-7ecc4d452dca'
+    'mypurecloud.com.au': '5783fd83-07c9-4f91-83b0-7ecc4d452dca',
+	'mypurecloud.de': 'a109ac66-7c61-4510-9502-5f9613e9093f'
 }
--- genesys/embrava_premium_client_app2/wizard/scripts/config.js
+++ genesys/embrava_premium_client_app2/wizard/scripts/config.js
@@ -2,7 +2,8 @@ export default {
     clientIDs: {
         'mypurecloud.com': '0b4127c4-6bd7-47a2-b9a2-e0fe274da287',
         'mypurecloud.ie': '1f2a0023-5d6d-4768-b39c-5093cf08a019',
-        'mypurecloud.com.au': 'a4349805-198e-406e-9e7a-14b48e6cdb9e'
+        'mypurecloud.com.au': 'a4349805-198e-406e-9e7a-14b48e6cdb9e',
+		'mypurecloud.de': '5b4f9571-1f45-4569-bce2-1b8bb47f480d'
     },
     "redirectUri": "https://embrava.github.io/genesys/embrava_premium_client_app2/wizard/index.html",
     
