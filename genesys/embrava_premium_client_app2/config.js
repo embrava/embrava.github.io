3d5677c9bbf2b3e9d209452bfba5eeb0908100f6
Senthil <43605449+embrava@users.noreply.github.com>
2022-05-03 09:05:38 +0530

    Update config.js

--- genesys/embrava_premium_client_app2/config.js
+++ genesys/embrava_premium_client_app2/config.js
@@ -1,5 +1,5 @@
 export default {
-    clientID: '168cb942-8b8a-4a5d-84e0-4c65c30d3e8e',
+    clientID: '168cb942-8b8a-4a5d-84e0-4c65c30d3e8f',
     basePath: 'https://embrava.github.io/genesys/embrava_premium_client_app2/',
 	appName: 'premium-app-embrava'
 }
