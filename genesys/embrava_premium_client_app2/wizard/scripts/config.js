13509bd6c4ca4b85a2e931492c5ce8850c7e0680
Senthil <43605449+embrava@users.noreply.github.com>
2022-05-03 10:33:43 +0530

    Update config.js

--- genesys/embrava_premium_client_app2/wizard/scripts/config.js
+++ genesys/embrava_premium_client_app2/wizard/scripts/config.js
@@ -1,6 +1,6 @@
 export default {
     clientID: '168cb942-8b8a-4a5d-84e0-4c65c30d3e8f',
-    redirectUri: "https://embrava.github.io/genesys/embrava_premium_client_app2/wizard/index.html",	
+    redirectUri: "https://embrava.github.io/genesys/embrava_premium_client_app2/index.html",	
 	appName: 'premium-app-embrava',
     
     //Permissions required for running the Wizard App
