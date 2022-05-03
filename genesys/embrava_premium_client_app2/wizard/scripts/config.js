246e45cef20a3ccb77cef44e3cb080f1c89ef00d
Senthil <43605449+embrava@users.noreply.github.com>
2022-05-03 10:36:14 +0530

    Update config.js

--- genesys/embrava_premium_client_app2/wizard/scripts/config.js
+++ genesys/embrava_premium_client_app2/wizard/scripts/config.js
@@ -1,6 +1,6 @@
 export default {
-    clientID: '168cb942-8b8a-4a5d-84e0-4c65c30d3e8f',
-    redirectUri: "https://embrava.github.io/genesys/embrava_premium_client_app2/index.html",	
+    clientID: 'e93d91c6-481d-4238-835d-566c425aab46',
+    redirectUri: 'https://embrava.github.io/genesys/embrava_premium_client_app/wizard/index.html',	
 	appName: 'premium-app-embrava',
     
     //Permissions required for running the Wizard App
@@ -9,5 +9,5 @@ export default {
     // To be added to names of PureCloud objects created by the wizard
     //"prefix": "STATUS_LIGHTS_FOR_PURECLOUD_",
     prefix: "Status Lights for PureCloud ",
-    prefixAppName: "Status Lights",
+    prefixAppName: "Status Lights"
 }
