bed2739e44fd439547b447e0972b41d558a88396
senthil-embrava <embrava.production@gmail.com>
2022-09-19 09:37:59 +0530

    Adding v2.9 Finesse gadget

---
+++ embrava/finesse/gadgets/v2.9/Config.js
@@ -0,0 +1,26 @@
+/*
+ * Embrava Connect address (Please use https://httpbin.org/get for test purposes)
+ */
+
+var embravaConnectURL = "https://localhost:8445/";
+
+ /*
+ * Show activate dashboard checkbox
+ */
+ var enableEmbravaConnector = false;
+ 
+/*
+ * Embrava Connector windows service address
+ */
+
+var embravaConnectorURL = "https://wkst-3.dcloud.cisco.com:8446/";
+ 
+ /*
+ * Show only the following scopes in UI. Possible options: Team, Queue, Agent. For example: ['Team', 'Queue', 'Agent']
+ */
+ var availableScopes = ['Queue'];
+ 
+ /*
+ * Show debug console
+ */
+ var debug = true;
